566e8f03c9afac60f390c317cb741bf799639233
DylanPrimera <dylanprimera98@gmail.com>
2024-11-08 11:57:23 -0500

    layout redirect validation and showing the user info

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,18 @@
 import { Sidebar, TopMenu } from "@/components";
+import { auth } from "../../../auth";
+import { redirect } from "next/navigation";
 
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  const session = await auth();
+  if(!session?.user) {
+    redirect('/api/auth/signin');
+  }
   return (
     <>
       <Sidebar />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,6 +9,7 @@ import {
 } from "react-icons/io5";
 
 import { PiCookieDuotone } from "react-icons/pi";
+import { auth, signOut } from "../../auth";
 
 const sidebarItems = [
   {
@@ -38,32 +39,45 @@ const sidebarItems = [
   },
 ];
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const session = await auth();
+  if (!session?.user) {
+    return null;
+  }
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
         <div className="-mx-6 px-6 py-4">
-          <a href="#" title="home">
-            <Image
-              src="https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg"
-              className="w-32"
-              alt="tailus logo"
-              width={32}
-              height={32}
-            />
-          </a>
+          <Image
+            src="https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg"
+            className="w-32"
+            alt="tailus logo"
+            width={32}
+            height={32}
+          />
         </div>
 
         <div className="mt-8 text-center">
-          <Image
-            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
-            alt=""
-            className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
-            width={150}
-            height={150}
-          />
+          {session?.user !== undefined ? (
+            <Image
+              src={session?.user?.image as string}
+              alt={session?.user?.name as string}
+              width={150}
+              height={150}
+              className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
+            />
+          ) : (
+            <Image
+              src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+              alt=""
+              className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
+              width={150}
+              height={150}
+            />
+          )}
+
           <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-            Cynthia J. Watts
+            {session?.user?.name !== null ? session?.user?.name : "Not logged"}
           </h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
@@ -75,12 +89,18 @@ export const Sidebar = () => {
         </ul>
       </div>
 
-      <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
+      <form
+        className="px-6 -mx-6 pt-4 flex justify-between items-center border-t"
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
         <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
           <CiLogout />
           <span className="group-hover:text-gray-700">Logout</span>
         </button>
-      </div>
+      </form>
     </aside>
   );
 };
